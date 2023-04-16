a48cb1c97604f3e7ec43cdafee751830dff4b3a4
Tomass50 <artem501as@gmail.com0>
2023-04-16 18:26:36 +0300

    add btn fucntion

--- Maket/JS/script.js
+++ Maket/JS/script.js
@@ -1,7 +1,24 @@
-const burger = document.querySelector('.burger');
+const burger = document.querySelector(".burger");
 const burgerPanel = document.querySelector(".burger-panel");
 console.log(burger);
-burger.addEventListener("click",() =>{
-    burger.classList.toggle("active")
-    burgerPanel.classList.toggle("active")
-})
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  burgerPanel.classList.toggle("active");
+});
+const header = document.querySelector(".header");
+
+console.log(header);
+window.addEventListener("scroll", (e) => {
+  if (window.pageYOffset === 0) {
+    header.classList.remove("scrolled");
+  } else {
+    header.classList.add("scrolled");
+  }
+});
+
+const section1Body = document.querySelector(".section1-body");
+const section1Button = document.querySelector(".section1-body button");
+section1Body.addEventListener("click", (e) => {
+  section1Button.classList.toggle("active");
+});
+console.log(section1Button);
